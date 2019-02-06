c23305fa3f7aaa0f4b2479b3a431b3da6409e980
Claire Sinozich <claire.sinozich@gmail.com>
2019-02-05 20:26:00 -0700

    I don't know what's happening

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,28 +1,12 @@
 const toggleMenu = () => {
-  menu.classList.toggle('.menu');
+  menu.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
-console.log(menu);
+//console.log(menu);
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
-console.log(menuButton);
+//console.log(menuButton);
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', this.toggleMenu);
-
-
-
-// class menuButtons {
-//
-//   constructor(newButton) {
-//     this.menu = newButton.querySelector('.menu');
-//     this.menuButton = newButton.querySelector('.menu-button')
-//     this.menuButton.addEventListener('click', this.toggleMenu)
-//   }
-//
-//   toggleMenu() {
-//     this.menuButton.classList.toggle('.menu')
-//   }
-//
-// }
