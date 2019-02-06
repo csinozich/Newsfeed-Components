1a5b56585b69accb97cbaf57ed5cde71ea13f185
Claire Sinozich <claire.sinozich@gmail.com>
2019-02-05 20:54:00 -0700

    Close Button Stretch

--- Article/Article.js
+++ Article/Article.js
@@ -15,7 +15,12 @@ class Article {
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
+    if (this.domElement.classList.toggle('article-open')) {
+      this.expandButton.textContent = 'close';
+    }
+    else {
+      this.expandButton.textContent = 'expand';
+    };
   }
 }
 
